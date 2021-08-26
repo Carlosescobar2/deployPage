2669536c6ff047a884e40f425e0fdac4f2ceb27b
Carlos Escobar <escobarcarlos@gmail.com>
2021-08-26 16:58:43 -0600

    deploy-pafe-2

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const app = express()
 app.get('/', function(req,res){ 
     res.sendFile(path.join(__dirname,'../index.html'))
 })
+
+app.use("/css",express.static(path.join(__dirname,"../style.css")))
+
 const port = process.env.PORT || 4005
 
 app.listen(port, ()=> { 
